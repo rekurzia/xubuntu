e87ea47fa1dbb7a2fe76a5b220e857c604f99fbe
vlado <ujovlado@gmail.com>
2016-11-17 20:10:52 +0100

    Remove upgrade part

--- base.sh
+++ base.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 apt-get update -q \
-  && apt-get upgrade \
   && apt-get install vim mc htop apg unrar curl tree dstat whois host ncdu git mercurial subversion meld ssh sshfs curlftpfs ftp-ssl nfs-common cifs-utils terminator chromium-browser mplayer keepassx \
   && apt-get autoremove
 
